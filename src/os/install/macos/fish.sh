69c0d17d4110af6ad767b75b884db9f6e501c9e7
tktcorporation <tktcorporation.go@gmail.com>
2021-02-02 21:15:20 +0900

    :+1: Add packages of docker and fish to macos

---
+++ src/os/install/macos/fish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Shells\n"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+printf "\n"
+
+brew_install "Fish" "fish"
