d891ff6a65f0319fd37e7ca625c0af45c52f6ed4
tktcorporation <tktcorporation.go@gmail.com>
2021-02-02 23:43:15 +0900

    :bug: Use `execute` command instead of `exec`

--- src/os/install/ubuntu/shell-commands.sh
+++ src/os/install/ubuntu/shell-commands.sh
@@ -12,8 +12,8 @@ print_in_purple "\n   Shell Commands\n\n"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_fzf() {
-    exec "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
-    exec "~/.fzf/install"
+    execute "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
+    execute "~/.fzf/install"
 }
 
 main() {
