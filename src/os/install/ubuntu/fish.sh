8faed0037f7ebd67578e0e80f1535b0ef1d8cd42
tktcorporation <tktcorporation.go@gmail.com>
2021-02-03 00:11:37 +0900

    :bug: Use fisher commands in fish shell

--- src/os/install/ubuntu/fish.sh
+++ src/os/install/ubuntu/fish.sh
@@ -25,20 +25,12 @@ install_fisher() {
         "curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish"
 }
 
-install_fisher_plugin() {
-    execute \
-        "fisher install decors/fish-ghq \
-        && fisher install jethrokuan/fzf \
-        && fisher install jethrokuan/zz"
-}
-
 main() {
 
     print_in_purple "\n   Fish\n\n"
 
     install_fish
     install_fisher
-    install_fisher_plugin
 }
 
 main
---
+++ src/os/install/ubuntu/fisher-plugins.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/fish
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Fisher\n\n"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_fisher_plugins() {
+    execute \
+        "fisher install decors/fish-ghq \
+        && fisher install jethrokuan/fzf \
+        && fisher install jethrokuan/zz"
+}
+
+main() {
+    install_fisher_plugin
+}
+
+main
