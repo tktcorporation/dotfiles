4c08bb6f0d6e8e99d3902eb703a5727c61dcd944
tktcorporation <tktcorporation.go@gmail.com>
2021-02-04 00:45:44 +0900

    :+1: Add alias sync for fish

--- src/shell/config.fish
+++ src/shell/config.fish
@@ -9,3 +9,8 @@ set -g theme_display_git_master_branch yes  # git の branch 名を表示
 set -g theme_color_scheme dracula
 set -g theme_display_date no  # 時刻を表示しないように設定
 set -g theme_display_cmd_duration no  # コマンド実行時間の非表示
+
+# ----------
+# bash aliases
+# ----------
+make -C "{$HOME}/.Bash2FishAliasesSync" sync _B2F_BASHRC="{$HOME}/.bash_aliases"; and source ~/.config/fish/b2f_aliases.fish
