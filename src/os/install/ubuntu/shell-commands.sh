d0e33999b85ad3375e6ca92aa68d3ed37a093ccf
tktcorporation <tktcorporation.go@gmail.com>
2021-02-03 20:54:56 +0900

    :sparkles: Add execute description of fzf

--- src/os/install/ubuntu/shell-commands.sh
+++ src/os/install/ubuntu/shell-commands.sh
@@ -12,8 +12,12 @@ print_in_purple "\n   Shell Commands\n\n"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_fzf() {
-    execute "git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf"
-    execute "$HOME/.fzf/install"
+    execute \
+        "git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf" \
+        "Download fzf"
+    execute \
+        "$HOME/.fzf/install" \
+        "Install fzf"
 }
 
 main() {
