80400433aaa102f9d0670a3c60dfa339b50d0970
tktcorporation <tktcorporation.go@gmail.com>
2021-02-07 04:25:22 +0900

    :bug: Fix vscode installation

--- src/os/install/ubuntu/git-kraken.sh
+++ src/os/install/ubuntu/git-kraken.sh
@@ -10,16 +10,7 @@ print_in_purple "\n   GitKraken\n\n"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_gitkraken() {
-    w_get "https://release.gitkraken.com/linux/gitkraken-amd64.deb" \
-        || print_error "gitkraken (wget)"
-    execute \
-        "sudo dpkg -i gitkraken-amd64.deb" \
-        "Install GitKraken"
-}
-
-main() {
-    install_gitkraken
-}
-
-main
+install_dep \
+    "https://release.gitkraken.com/linux/gitkraken-amd64.deb" \
+    "GitKraken" \
+    "gitkraken"
