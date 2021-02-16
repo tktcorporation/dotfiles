37dd316c1c644e7de232a9491beccc403fe4eec1
tktcorporation <tktcorporation.go@gmail.com>
2021-02-16 20:59:22 +0900

    :+1: Install ClamAV

--- src/os/install/ubuntu/misc_tools.sh
+++ src/os/install/ubuntu/misc_tools.sh
@@ -11,6 +11,8 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 install_package "cURL" "curl"
 install_package "ShellCheck" "shellcheck"
 install_package "xclip" "xclip"
+install_package "ClamAV" "clamav"
+
 
 # if [ -d "$HOME/.nvm" ]; then
 
