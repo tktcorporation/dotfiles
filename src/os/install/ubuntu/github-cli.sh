7ff2777a99294c0f58b893b94318f9869128d618
tktcorporation <tktcorporation.go@gmail.com>
2022-02-28 14:29:46 +0900

    :sparkles: install gh with brew

--- src/os/install/ubuntu/github-cli.sh
+++ src/os/install/ubuntu/github-cli.sh
@@ -11,23 +11,10 @@ print_in_purple "\n   GithubCLI\n\n"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_githubcli() {
-    if ! package_is_installed "gh"; then
-        execute \
-            "sudo apt-key adv \
-                --keyserver keyserver.ubuntu.com \
-                --recv-key C99B11DEB97541F0" \
-            &> /dev/null \
-            || print_error "Github CLI (add key)"
-
-        add_repo "https://cli.github.com/packages" \
-            || print_error "Github CLI (add to package resource list)"
-
-        update &> /dev/null \
-            || print_error "Github CLI (resync package index files)"
-
-    fi
-
-    install_package "Github CLI" "gh"
+    execute \
+        "brew install gh" \
+        "Install Github CLI(gh)" \
+        || print_error "gh (brew install)"
 }
 
 install_extensions() {
